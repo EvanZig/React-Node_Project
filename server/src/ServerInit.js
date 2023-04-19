02ca969871f11116dd3266ee9f0416be90d9a780
Evangelos Zigkas <82516621+EvanZig@users.noreply.github.com>
2023-04-19 16:26:16 +0300

    connected to sql workbench

--- server/src/ServerInit.js
+++ server/src/ServerInit.js
@@ -3,14 +3,24 @@ const deleteRouter = require("./apiCalls/delete");
 const showRouter = require("./apiCalls/show");
 const updateRouter = require("./apiCalls/update");
 const registerRouter = require("./apiCalls/register");
+const bodyParser = require("body-parser");
 const app = express();
 
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   database: "",
   user: "root",
-  password: "kostasvai2001",
+  password: "123456",
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error("Failed to connect to the MySQL database:", error);
+  } else {
+    console.log("Connected to the MySQL database!");
+    // Perform your database queries here...
+  }
 });
 
 app.get("/", (req, res) => {
