92dc63f7f2c409435373c84b7c6433d7e9b88515
GitSkrud <euaggeloszigas@gmail.com>
2023-04-22 03:18:11 +0300

    getUsers api

--- server/src/userApiCalls/show.js
+++ server/src/userApiCalls/show.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const getConnection = require("../getConnection");
 
-router.get("/", (req, res) => {
-  res.send("show smth");
+router.get("/", async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const query = "SELECT * FROM users ";
+    connection.query("SELECT * FROM users", function (error, results, fields) {
+      if (error) throw error;
+      const users = JSON.stringify(results);
+      res.json(users);
+    });
+
+    res.sendStatus(200);
+    connection.release();
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
